dc555c5a0c04926502c297a9461ad09881d442d0
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-01-09 19:57:05 +0200

    Created for loop for translation examples

--- src/components/Term.vue
+++ src/components/Term.vue
@@ -2,12 +2,12 @@
     <v-container>
 
         <v-layout>
-            <v-flex class='mb-3'>
+            <v-flex>
                 <h1>Pali <v-icon>play_arrow</v-icon></h1>
             </v-flex>
         </v-layout>
 
-        <v-layout v-for='meaning in meanings'>
+        <v-layout v-for='meaning in meanings' class='mt-3'>
             <v-flex>
                 <h3>{{ meaning.est }} [skr {{ meaning.root }}]</h3>
                 <p><i>{{ meaning.eng }}</i></p>
@@ -17,8 +17,14 @@
                         <div slot="header">Täpsemalt</div>
                         <v-card>
                             <v-card-text class="grey lighten-4">
-                                {{ meaning.further }}
-                                <div>{{ meaning.examples }}</div>
+                                <p>{{ meaning.further }}</p>
+                                <h4>Tõlkenäited:</h4>
+                                <div>
+                                    <p v-for='example in meaning.examples'>
+                                        <i>{{ example.original }}</i><br>
+                                        {{ example.translation }}
+                                    </p>
+                                </div>
                             </v-card-text>
                         </v-card>
                     </v-expansion-panel-content>
@@ -34,7 +40,7 @@
     data () {
       return {
         meanings: [
-            { est: "esimene tõlge", root: "skrskr", eng: 'in English', expl: 'Siin on selgitud', further: 'more info', examples: 'fgh' },
+            { est: "esimene tõlge", root: "skrskr", eng: 'in English', expl: 'Siin on selgitud', further: 'more info', examples: [{ original: 'in pali', translation: 'eestikeelne tõlge'}, { original: 'more in pali', translation: 'veel üks eestikeelne tõlge' } ] },
             { est: "teine tõlge", root: "skrskr", eng: 'in English', expl: 'Siin on veel üks selgitus', further: 'even more info' },
 
         ]
