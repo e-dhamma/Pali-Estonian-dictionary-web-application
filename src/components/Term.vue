f74df79680bbbbf77e44ab851b13039813b0c00f
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-01-10 14:46:48 +0200

    Added multiple eng translations possibility

--- src/components/Term.vue
+++ src/components/Term.vue
@@ -3,14 +3,14 @@
 
         <v-layout>
             <v-flex>
-                <h1><i>Pali</i><v-icon>play_arrow</v-icon></h1>
+                <h1><i>{{ term.pali }}</i><v-icon>play_arrow</v-icon></h1>
             </v-flex>
         </v-layout>
 
-        <v-layout v-for='(meaning, i) in meanings' class='mt-3'>
+        <v-layout v-for='(meaning, i) in term.meanings' class='mt-3'>
             <v-flex>
                 <h3>{{ i + 1}}. {{ meaning.est }} [skr <i>{{ meaning.root }}</i>]</h3>
-                <p><i>{{ meaning.eng }}</i></p>
+                <p><template v-for='e in meaning.eng'><i>{{ e }}</i>, </template></p>
                 <p>{{ meaning.expl }}</p>
                 <v-expansion-panel>
                     <v-expansion-panel-content>
@@ -32,7 +32,7 @@
             </v-flex>
         </v-layout>
         <hr class='mt-4'>
-        <div v-for='(comment, i) in comments' :key='i' class='mt-2'>
+        <div v-for='(comment, i) in term.comments' :key='i' class='mt-2'>
             <b>{{ comment.author }}:</b> {{ comment.content}}<br>{{ comment.date }}
             </div>
 
@@ -44,14 +44,17 @@
   export default {
     data () {
         return {
-            meanings: [
-                { est: "esimene tõlge", root: "skrskr", eng: 'in English', expl: 'Siin on selgitud', further: 'more info', examples: [{ original: 'in pali', translation: 'eestikeelne tõlge'}, { original: 'more in pali', translation: 'veel üks eestikeelne tõlge' } ] },
-                { est: "teine tõlge", root: "skrskr", eng: 'in English', expl: 'Siin on veel üks selgitus', further: 'even more info' },
-            ],
-            comments: [
-                {author: 'Mari', date: '10.10.2017', content: 'Siin on kommentar.' },
-                {author: 'Juhan', date: '17.10.2017', content: 'Siin on teine kommentar.' }
-            ]     
+            term: {
+                pali: 'paali keeles',
+                meanings: [
+                    { est: "esimene tõlge", root: "skrskr", eng: ['one English version', 'another translation'], expl: 'Siin on selgitud', further: 'more info', examples: [{ original: 'in pali', translation: 'eestikeelne tõlge'}, { original: 'more in pali', translation: 'veel üks eestikeelne tõlge' } ] },
+                    { est: "teine tõlge", root: "skrskr", eng: ['one English version', 'another translation'], expl: 'Siin on veel üks selgitus', further: 'even more info' },
+                ],
+                comments: [
+                    {author: 'Mari', date: '10.10.2017', content: 'Siin on kommentar.' },
+                    {author: 'Juhan', date: '17.10.2017', content: 'Siin on teine kommentar.' }
+                ]
+            }    
         }
     }
   }
