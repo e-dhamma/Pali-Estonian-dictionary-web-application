2034c93cb91bde6d33552b48734cc313f902b76a
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-01-14 14:17:05 +0200

    Altered date filter

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,4 +1,4 @@
 export default (value) => {
   const date = new Date(value)
-  return date.toLocaleString()
+  return date.toLocaleString('et-ee', {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})
 }
