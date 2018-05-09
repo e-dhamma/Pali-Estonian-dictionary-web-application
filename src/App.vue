00820380edf7b8685208896ff4c43c061ae8a260
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-05-09 14:47:32 +0300

    Create NavigationDrawer component (close #13)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <template>
   <v-app>
     <!-- Term-list -->
-    <v-navigation-drawer
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      v-model="drawer"
-      fixed
-      app
-      width="200"
-      class="brown lighten-5"
-    >
-      <v-list>
-        <v-list-tile v-for="term in termList" :key="term.id">
-          <v-list-tile-title>
-            <router-link :to="'/terminid/' + term.slug" class="term-list-item">
-              <span class="term-list-item">{{term.pali}}</span>
-            </router-link>
-          </v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <navigation-drawer></navigation-drawer>
     <!-- Toolbar -->
     <v-toolbar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
@@ -83,6 +66,7 @@
 </template>
 
 <script>
+  import NavigationDrawer from './components/NavigationDrawer'
   export default {
     data () {
       return {
@@ -102,13 +86,8 @@
         // ]
       }
     },
-    created () {
-      this.$store.dispatch('addTermList')
-    },
-    computed: {
-      termList () {
-        return this.$store.getters.termList
-      }
+    components: {
+      'navigation-drawer': NavigationDrawer
     },
     methods: {
       showNotification (notification) {
@@ -132,10 +111,3 @@
     }
   }
 </script>
-
-<style scoped>
-.term-list-item {
-  color: #cd5a07;
-  text-decoration: none
-}
-</style>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-navigation-drawer
+    :clipped="$vuetify.breakpoint.lgAndUp"
+    v-model="drawer"
+    fixed
+    app
+    width="200"
+    class="brown lighten-5"
+  >
+    <v-list>
+      <v-list-tile v-for="term in termList" :key="term.id">
+        <v-list-tile-title>
+          <router-link :to="'/terminid/' + term.slug" class="term-list-item">
+            <span class="term-list-item">{{term.pali}}</span>
+          </router-link>
+        </v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+      }
+    },
+    created () {
+      this.$store.dispatch('addTermList')
+    },
+    computed: {
+      termList () {
+        return this.$store.getters.termList
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.term-list-item {
+  color: #cd5a07;
+  text-decoration: none
+}
+</style>
