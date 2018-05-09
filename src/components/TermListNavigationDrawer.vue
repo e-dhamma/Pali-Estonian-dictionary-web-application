ccf2a5b138a52b7c810873c6d1794f9a2a698c43
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-05-09 23:54:29 +0300

    Trying to figure out scrolling, but not there yet

--- src/components/TermListNavigationDrawer.vue
+++ src/components/TermListNavigationDrawer.vue
@@ -7,11 +7,34 @@
     width="200"
     class="brown lighten-5"
   >
-    <v-list>
-      <v-list-tile v-for="term in termList" :key="term.id">
-        <v-list-tile-title>
+    <v-list ref="termListNavigationDrawer">
+      <!-- <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile>
+      <v-list-tile><v-list-tile-title>foo</v-list-tile-title> </v-list-tile> -->
+      <v-list-tile v-for="term in termList" :key="term.id" :id="term.slug">
+        <v-list-tile-title :id="term.slug">
           <router-link :to="'/terminid/' + term.slug" class="term-list-item">
-            <span class="term-list-item">{{term.pali}}</span>
+            <span class="term-list-item" :id="term.slug">{{term.pali}}</span>
           </router-link>
         </v-list-tile-title>
       </v-list-tile>
@@ -29,11 +52,27 @@
     created () {
       this.$store.dispatch('addTermList')
     },
+    updated () {
+      this.autoScrollToViewedTerm('buddha')
+    },
+    methods: {
+      autoScrollToViewedTerm (slug) {
+        const selectedElement = document.getElementById(slug)
+        if (!selectedElement) {
+          return
+        }
+        const toolbarHeight = 64
+        this.$refs.termListNavigationDrawer.$el.scrollTop = 445
+        console.log(selectedElement)
+        console.log(this.$refs['termListNavigationDrawer'].$el);
+      }
+    },
     computed: {
       termList () {
         return this.$store.getters.termList
       }
     }
+
   }
 </script>
 
