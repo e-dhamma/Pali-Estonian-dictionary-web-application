70aba10d552fa4805ef4fb8c0d872ff49241f499
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-05-10 22:48:55 +0300

    Position footer centrally in relation to main content div

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,52 @@
 <template>
-  <v-container>
+  <div>
+    <v-container>
 
-    <v-layout row class='mt-5'>
-          <v-flex class='text-xs-center'>
-            <img src="/static/img/DharmaWheel.827aa0e.png" alt="logo" width='150'>
-          </v-flex>
-    </v-layout>
+      <v-layout row class='mt-5'>
+        <v-flex class='text-xs-center'>
+          <img src="/static/img/DharmaWheel.827aa0e.png" alt="logo" width='150'>
+        </v-flex>
+      </v-layout>
 
-    <v-layout row wrap align-center>
-      <v-flex>
-        <v-text-field
-        value="Otsi"
-        style="float:left">
-        </v-text-field>
-      </v-flex>
-      <v-flex>
-        <v-btn color="primary"><v-icon>search</v-icon></v-btn>
-      </v-flex>
-    </v-layout>
+      <v-layout row wrap align-center>
+        <v-flex>
+          <v-text-field
+          value="Otsi"
+          style="float:left">
+          </v-text-field>
+        </v-flex>
+        <v-flex>
+          <v-btn color="primary"><v-icon>search</v-icon></v-btn>
+        </v-flex>
+      </v-layout>
 
-    <v-layout row wrap class='mt-2'>
-      <v-flex class='text-xs-center'>
-        <p class='intro'>Budistliku õpetuse edasiandmiseks loodud mahukas tekstikorpus kätkeb endas keerukat terminoloogiat, mille tõlkimine
-            emakeelde on läbi aegade olnud oluliseks sammuks Buddha õpetuse juurdumisel kohalikus kultuuriruumis.
-            Käesolev sõnaraamat on mõeldud nii budistliku kirjanduse lugejatele kui tõlkijatele, aidates leida
-            terminitele selgitusi ja tõlkevasteid ning panustades ühtse eestikeelse terminoloogia väljakujunemisse.</p>
-      </v-flex>
-    </v-layout>
+      <v-layout row wrap class='mt-2'>
+        <v-flex class='text-xs-center'>
+          <p class='intro'>Budistliku õpetuse edasiandmiseks loodud mahukas tekstikorpus kätkeb endas keerukat terminoloogiat, mille tõlkimine
+              emakeelde on läbi aegade olnud oluliseks sammuks Buddha õpetuse juurdumisel kohalikus kultuuriruumis.
+              Käesolev sõnaraamat on mõeldud nii budistliku kirjanduse lugejatele kui tõlkijatele, aidates leida
+              terminitele selgitusi ja tõlkevasteid ning panustades ühtse eestikeelse terminoloogia väljakujunemisse.</p>
+        </v-flex>
+      </v-layout>
 
-    <v-layout row wrap class='mt-2'>
-      <v-flex class='text-xs-center text-sm-right'>
-        <v-btn  to="/kasutajale">Teave kasutajale</v-btn>
-      </v-flex>
-      <v-flex class='text-xs-center text-sm-left'>
-        <v-btn  to="/">Teine nupp</v-btn>
-      </v-flex>
-    </v-layout>
+      <v-layout row wrap class='mt-2'>
+        <v-flex class='text-xs-center text-sm-right'>
+          <v-btn  to="/kasutajale">Teave kasutajale</v-btn>
+        </v-flex>
+        <v-flex class='text-xs-center text-sm-left'>
+          <v-btn  to="/">Teine nupp</v-btn>
+        </v-flex>
+      </v-layout>
 
-    <v-layout>
-    <div class="footer">
+    </v-container>
+
+    <v-footer inset app>
       <div style="margin: auto">
-      <p style="text-allign: center">See on avatud lähtekoodiga projekt. Vaata lähtekoodi <a href="https://github.com/e-dhamma/Pali-Estonian-dictionary-web-application">siit</a>.</p>
+        <p style="text-allign: center">See on avatud lähtekoodiga projekt. Vaata lähtekoodi <a href="https://github.com/e-dhamma/Pali-Estonian-dictionary-web-application">siit</a>.</p>
       </div>
-    </div>
-    </v-layout>
-  </v-container>
+    </v-footer>
+
+  </div>
 </template>
 
 <style scoped>
@@ -53,12 +55,5 @@
   }
   .btn {
     min-width: 40px;
-  }
-  .footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    text-align: center;
   }
 </style>
